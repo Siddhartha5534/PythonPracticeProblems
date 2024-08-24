b90efdf055cdc6d5e1f5b184ff0977e7aab25510
siddharthachaudhury <streetcustoms8@gmail.com>
2024-08-23 20:31:05 -0400

    Comments

--- PythonPracticeProblems.py
+++ PythonPracticeProblems.py
@@ -1,21 +1,29 @@
+#problem 1
+# Complete the function so that it finds the average of the three scores passed to it and returns the letter value associated with that grade.
+# Numerical Score	Letter Grade
+# 90 <= score <= 100	'A'
+# 80 <= score < 90	'B'
+# 70 <= score < 80	'C'
+# 60 <= score < 70	'D'
+# 0 <= score < 60	'F'
+#solution
 # Initialize an empty list to store the numbers
 numbers = []
 while True:
     user_input = input("Please enter a number: ") # asking for an user input
     try:
-        # Try to convert the input to a float
-        number = float(user_input) 
+        number = float(user_input) # Try to convert the input to a float
         print(f"You entered the number: {number}")
-        if number < 100 :
-            numbers.append(number)
-            if len(numbers) == 3 :
-                print(f"The grades that has been provided are : {numbers}")
-                break  # Exit the loop if the input is a valid number
+        if number <=  100 : # checking if the number is less than 100
+            numbers.append(number) # if the previous line is true , appending the number to the list
+            if len(numbers) == 3 : # checking the length of the list - restricted to 3
+                print(f"The grades that has been provided are : {numbers}") 
+                break  # Exit the loop if the input is a valid number # if 3 numbers are added to the list break out of the while loop
         else:
-            print("The number should be less than 100. Please try again.")
+            print("The number should be less than 100. Please try again.") # letting the user know that the number is not less than or equal to 100
     except ValueError:
         # If the input is not a valid number, print an error and continue the loop
-        print("Invalid input. Please enter a valid number.")
+        print("Invalid input. Please enter a valid number.") # catching the error raised in the "try" block
 average_grade = sum(numbers)/len(numbers)
 if average_grade >= 90 and average_grade <= 100 :
     print(f"Grade A  : you're awesome")
