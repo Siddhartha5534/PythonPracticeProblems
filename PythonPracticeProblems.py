d821da4acae7875551db2ef04309764bf3612038
Siddhartha5534 <97868019+Siddhartha5534@users.noreply.github.com>
2024-08-27 11:26:00 -0400

    Update

--- PythonPracticeProblems.py
+++ PythonPracticeProblems.py
@@ -129,6 +129,7 @@ while number <= 10:
 """
 #problem 6
 #Write a function to check palindrome number
+"""
 def palindromenumber():
     while True:
         user_input = input("Enter a number of your choice ") # asking for an user input
@@ -152,4 +153,26 @@ def palindromenumber():
             print("Invalid input. Please enter a valid number.") # catching the error raised in the "try" block
 
 print(palindromenumber())
+"""
+"""
+def palindromenumber():
+    while True:
+        user_input = input("Enter a number of your choice ") # asking for an user input
+        try:
+            number = int(user_input) # Try to convert the input to an int
+            revised_number_as_string = str(number)[::-1]
+            revised_number = int(revised_number_as_string)
+            if number == revised_number :
+                print("palindrome number")
+                return revised_number
+                break
+            else :
+                print("Not a palindrome number")
+                return revised_number
+                break
+        except ValueError: # If the input is not a valid number, print an error and continue the loop
+            print("Invalid input. Please enter a valid number.") # catching the error raised in the "try" block
+
+palindromenumber()
+"""
 
